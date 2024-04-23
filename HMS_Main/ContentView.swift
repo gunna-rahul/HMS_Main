2d9068d7339e02e69c416455d1d6a3866170e219
gunna-rahul <grk25303@gmail.com>
2024-04-23 13:30:14 +0530

    Initial Commit

---
+++ HMS_Main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HMS_Main
+//
+//  Created by admin on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HMS_Main/HMS_MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMS_MainApp.swift
+//  HMS_Main
+//
+//  Created by admin on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HMS_MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
